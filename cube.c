bcd9bc6a15da40a1631f682d6c853f2cb7d17221
Laddge <dev.laddge@gmail.com>
2021-12-24 08:37:22 +0900

    [feat] cubeという構造体を定義

--- cube.c
+++ cube.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/* cubeという構造体を定義 */
+typedef struct {
+    int cp[8]; /* Corner Permutation コーナーパーツの位置 */
+    int co[8]; /* Corner Orientation コーナーパーツの向き */
+    int ep[12]; /* Edge Permutation エッジパーツの位置 */
+    int eo[12]; /* Edge Orientation エッジパーツの向き */
+} cube;
+
 /* メイン関数 */
 int main(void) {
     printf("hello, world\n");
